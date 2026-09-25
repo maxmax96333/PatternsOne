89f067fbce92172eb1166934a7a384dcd093e69f
Max study netology <maxmax96333@gmail.com>
2026-09-25 12:27:03 +0300

    Fixing

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
 import static com.codeborne.selenide.Condition.text;
@@ -23,46 +22,31 @@ class DeliveryTest {
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
-
         var validUser = DataGenerator.Registration.generateUser("ru");
-
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
 
-        $("[data-test-id='city'] input")
-                .setValue(validUser.getCity());
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
 
-        $("[data-test-id='date'] input")
-                .press(Keys.chord(Keys.CONTROL, "A"))
-                .setValue(firstMeetingDate);
+        $("[data-test-id='date'] input").clear();
+        $("[data-test-id='date'] input").setValue(firstMeetingDate);
 
-        $("[data-test-id='name'] input")
-                .setValue(validUser.getName());
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
 
-        $("[data-test-id='phone'] input")
-                .setValue(validUser.getPhone());
-
-        $("[data-test-id='agreement']")
-                .click();
-
-        $$("button")
-                .findBy(text("Запланировать"))
-                .click();
+        $$("button").findBy(text("Запланировать")).click();
 
         $("[data-test-id='success-notification']")
                 .shouldBe(visible)
                 .shouldHave(text("Встреча успешно запланирована"));
 
-        $("[data-test-id='date'] input")
-                .press(Keys.chord(Keys.CONTROL, "A"))
-                .setValue(secondMeetingDate);
+        $("[data-test-id='date'] input").clear();
+        $("[data-test-id='date'] input").setValue(secondMeetingDate);
 
-        $$("button")
-                .findBy(text("Запланировать"))
-                .click();
+        $$("button").findBy(text("Запланировать")).click();
 
         $("[data-test-id='replan-notification']")
                 .shouldBe(visible)
@@ -73,6 +57,7 @@ class DeliveryTest {
         $("[data-test-id='replan-notification']")
                 .$$("button")
                 .findBy(text("Перепланировать"))
+                .shouldBe(Condition.enabled)
                 .click();
 
         $("[data-test-id='success-notification']")
